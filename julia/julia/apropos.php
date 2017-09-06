983e107f2eb46cab9eb29109417acff708bae5e8
jonathan <jonathan@pepe.lan>
2017-09-06 21:31:07 +0200

    suite2

---
+++ julia/julia/apropos.php
@@ -0,0 +1,1 @@
+<div id="apropos" class="right margtop"><img  src="/rsc/img/moi.JPG"/>Salut C'est moi Julia</div>
---
+++ julia/julia/contact.php
@@ -0,0 +1,6 @@
+<div id="contact" class="right margtop">
+    <div id="contact_email" class="center">
+       <a href="mailto:julia-lecorre@club-internet.com" >julia-lecorre@club-internet.com</a>
+    </div>
+    <img  src="/rsc/img/contact.jpg"/>
+</div>
